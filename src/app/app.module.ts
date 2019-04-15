af6de95d7e5a76119d51abd0dcf1210c42de39b4
AITCWEB <aitc.dev@gmail.com>
2019-04-15 09:44:53 +0200

    http

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,37 +1,41 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from "./app-routing.module";
-import { AppComponent } from "./app.component";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
 
-import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
-import { MatToolbarModule } from "@angular/material/toolbar";
-import { FlexLayoutModule } from "@angular/flex-layout";
-import { MatListModule } from "@angular/material/list";
-import { MatGridListModule } from "@angular/material/grid-list";
-import { MatCardModule } from "@angular/material/card";
-import { MatButtonModule } from "@angular/material/Button";
-import { MatDialogModule } from "@angular/material/dialog";
-import { MatFormFieldModule } from "@angular/material/form-field";
-import { MatInputModule } from "@angular/material/input";
-import { MatCheckboxModule } from "@angular/material/checkbox";
-import { FormsModule } from "@angular/forms";
-import { MatSelectModule } from "@angular/material/select";
-import { MatSlideToggleModule } from "@angular/material/slide-toggle";
-import { ReactiveFormsModule } from "@angular/forms";
-import "hammerjs";
-import { MenuComponent } from "./menu/menu.component";
-import { DishdetailComponent } from "./dishdetail/dishdetail.component";
-import { DishService } from "./services/dish.service";
-import { HeaderComponent } from "./header/header.component";
-import { FooterComponent } from "./footer/footer.component";
-import { HomeComponent } from "./home/home.component";
-import { AboutComponent } from "./about/about.component";
-import { ContactComponent } from "./contact/contact.component";
-import { PromotionService } from "./services/promotion.service";
-import { LeaderService } from "./services/leader.service";
-import { LoginComponent } from "./login/login.component";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/Button';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ReactiveFormsModule } from '@angular/forms';
+import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component';
+import { DishService } from './services/dish.service';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import { PromotionService } from './services/promotion.service';
+import { LeaderService } from './services/leader.service';
+import { LoginComponent } from './login/login.component';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatSliderModule } from '@angular/material/slider';
+import { HttpClientModule } from '@angular/common/http';
+import { baseURL } from './shared/baseurl';
+import { ProcessHTTPMsgService } from './services/process-httpmsg.service';
 
 @NgModule({
   declarations: [
@@ -43,7 +47,7 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     HomeComponent,
     AboutComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
   ],
   imports: [
     BrowserModule,
@@ -62,11 +66,22 @@ import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
     MatSelectModule,
     MatSlideToggleModule,
     MatProgressSpinnerModule,
+    MatSliderModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
-  providers: [DishService, PromotionService, LeaderService],
+  providers: [
+    DishService,
+    PromotionService,
+    LeaderService,
+    ProcessHTTPMsgService,
+    {
+      provide: 'BaseURL',
+      useValue: baseURL,
+    },
+  ],
   entryComponents: [LoginComponent],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, Inject } from '@angular/core';
 import { Dish } from '../shared/dish';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { DishService } from '../services/dish.service';
 import { switchMap } from 'rxjs/operators';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-dishdetail',
@@ -11,30 +12,95 @@ import { switchMap } from 'rxjs/operators';
   styleUrls: ['./dishdetail.component.scss'],
 })
 export class DishdetailComponent implements OnInit {
+  errMsg: string;
+
+  @ViewChild('cform') commentFormDirective;
+  commentForm: FormGroup;
+  commentValue: Comment;
+  value = '5';
   dish: Dish;
   dishIds: string[];
   prev: string;
   next: string;
+  dishcopy: Dish;
+  formErrors = {
+    author: '',
+    comment: '',
+    rating: '',
+  };
+  validationMessages = {
+    author: {
+      required: 'Author Name is required.',
+      minlength: 'Author Name must be at least 2 characters long.',
+    },
+    comment: {
+      required: 'First Name is required.',
+      minlength: 'First Name must be at least 2 characters long.',
+    },
+    rating: {
+      required: 'First Name is required.',
+    },
+  };
   constructor(
     private dishService: DishService,
     private route: ActivatedRoute,
     private location: Location,
-  ) {}
+    private fb: FormBuilder,
+    @Inject('BaseURL') private BaseURL,
+  ) {
+    this.createForm();
+  }
 
   ngOnInit() {
-    this.dishService.getDishIds().subscribe(ids => {
-      console.log(this.dishIds);
-      this.dishIds = ids;
-    });
+    this.dishService.getDishIds().subscribe(
+      ids => {
+        this.dishIds = ids;
+      },
+      errMsg => (this.errMsg = <any>errMsg),
+    );
 
     this.route.params
       .pipe(switchMap((params: Params) => this.dishService.getDish(params.id)))
-      .subscribe(dish => {
-        this.dish = dish;
-        this.setPrevNext(dish.id);
-      });
+      .subscribe(
+        dish => {
+          this.dish = dish;
+          this.dishcopy = dish;
+          this.setPrevNext(dish.id);
+        },
+        errMsg => (this.errMsg = <any>errMsg),
+      );
   }
-
+  createForm() {
+    this.commentForm = this.fb.group({
+      author: ['', [Validators.required, Validators.minLength(2)]],
+      comment: ['', [Validators.required, Validators.minLength(2)]],
+      rating: ['', Validators.required],
+      date: new Date().toISOString(),
+    });
+    this.commentForm.valueChanges.subscribe(data => this.onValueChange(data));
+    this.onValueChange(); // reset form validation messages
+  }
+  onValueChange(data?: any) {
+    if (!this.commentForm) {
+      return;
+    }
+    const form = this.commentForm;
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {
+        // clear previous error message ( if any)
+        this.formErrors[field] = '';
+        const control = form.get(field);
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) {
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
+  }
   setPrevNext(dishId: string) {
     const index = this.dishIds.indexOf(dishId);
     this.prev = this.dishIds[
@@ -47,5 +113,28 @@ export class DishdetailComponent implements OnInit {
 
   goBack() {
     this.location.back();
+  }
+  onSubmit() {
+    this.commentValue = this.commentForm.value;
+    console.log(this.commentValue);
+    this.dishcopy.comments.push(this.commentValue);
+    this.dishService.putDish(this.dishcopy).subscribe(
+      dish => {
+        this.dish = dish;
+        this.dishcopy = dish;
+      },
+      errMsg => {
+        this.dish = null;
+        this.dishcopy = null;
+        this.errMsg = <any>errMsg;
+      },
+    );
+    this.commentForm.reset({
+      author: '',
+      comment: '',
+      rating: '',
+      date: '',
+    });
+    this.commentFormDirective.resetForm();
   }
 }
